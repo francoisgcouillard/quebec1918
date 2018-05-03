10115ae01c8c0d799925e730b35d07d9b5f0c795
Francois couillard <fgosselin@cofomo.com>
2018-05-03 08:51:53 -0400

    Inclusion de la librairie Lang

--- js/lang.js
+++ js/lang.js
@@ -1,32 +1,55 @@
-var lang = {
-	langs : ['fr', 'en', 'es'],
-	langCode : '',
-	langJS : {},
-	langSet : 'en' //default language;
-	
-	
-};
-
-var translate = function (jsdata)
-{	
-	lang.langJS = jsdata;
-	
-	$("[tkey]").each (function () {
-		var strTr = jsdata [$(this).attr ('tkey')];
+class Lang {
+	constructor () {
+		this.langs = ['fr', 'en', 'it'];
+		this.langBrowser = '';
+		this.library = {}; //every words of the good lang
+		this.langSet = '';
 		
-	    $(this).html (strTr);
-	});
+		if (navigator.language.substr (0, 2) != undefined) {
+			this.langBrowser = navigator.language.substr (0, 2);
+			if (this.langs.includes(this.langBrowser)) 
+				this.langSet = this.langBrowser;
+		}
+	};
+	loadAndSet () {
+		$.getJSON('lang/'+this.langSet+'.json', function (jsdata) {
+			this.library = jsdata;
+		
+			$("[tkey]").each (function (index) {
+				if ( $(this).attr ('tkey').length )
+					var strTr = jsdata [$(this).attr ('tkey')];
+				else {
+					var strTr = jsdata [$(this).html()];
+					$(this).attr( 'tkey', $(this).html() );
+				}
+				
+				$(this).attr("lang", $("html").attr("lang"));
+				$(this).html (strTr);
+			});
+		}.bind(this));
+	};
+	//Get a div with translated text with all infos for future translate
+	getTextContainer (str) {
+		return ( $("<div/>").attr("tkey", str).attr("lang", this.langSet).html(this.getText(str)) );
+	};
+	getText(key) {
+		if (this.library[key] == undefined)
+			console.log ("Clé introuvable=" + key);
+		
+		return (this.library[key] == undefined ? key : this.library[key] );
+	};
+	setLang (lang) {
+		if (lang == undefined)
+			lang = 'fr';
+		
+		//If we have a library with this language
+		if (this.langs.includes(lang)) {
+			$("html").attr("lang", lang);
+			this.langSet = lang;
+			this.loadAndSet();
+		}
+	};
 }
 
-function getText(key) {
-	return (lang.langJS[key] == undefined ? key : lang.langJS[key] );
-}
-
-
-lang.langCode = navigator.language.substr (0, 2);
-
-if (lang.langCode in lang.langs)
-	lang.langSet = lang.langCode;
-
-$.getJSON('lang/'+lang.langSet+'.json', translate);
-
+var lang = new Lang();
+lang.setLang();
