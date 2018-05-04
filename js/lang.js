abc4b36250434f3d64fa56b8e4cceea391480328
Francois couillard <fgosselin@cofomo.com>
2018-05-04 09:08:46 -0400

    évolution lang 0.0.1

--- js/lang.js
+++ js/lang.js
@@ -28,15 +28,28 @@ class Lang {
 			classLang.library = jsdata;
 			
 			$("[data-tkey]").each (function (index) {
-				if ( $(this).attr ('data-tkey').length )
-					var strTr = classLang.getText($(this).attr ('data-tkey'));
-				else {
-					var strTr = classLang.getText($(this).html());
-					$(this).attr( 'data-tkey', $(this).html() );
+				//Get the key and set the text
+				
+				//If there's a value in the key attribute
+				if ( $(this).attr ('data-tkey').length ) { 
+					var strTr = classLang.getText( $(this).attr ('data-tkey') );
+					
+					( $(this)[0].tagName == "META" ? $(this).attr( 'content', strTr) : $(this).html (strTr) );
+				}
+				//if there is something inside the tag (html) then the tag is here
+				else if ( $(this).html().length ) { 
+					$(this).attr ( 'data-tkey', $(this).html() );
+					var strTr = classLang.getText( $(this).html() );
+					$(this).html ( strTr );
+				}
+				//if there is a content attribute
+				else if ( $(this).attr('content') ) { 
+					$(this).attr ( 'data-tkey', $(this).attr('content') );
+					var strTr = classLang.getText( $(this).attr('content') );
+					$(this).attr ( 'content', strTr );
 				}
 				
 				$(this).attr ("lang", $("html").attr("lang"));
-				$(this).html (strTr);
 			});
 		})
 		.fail (function() {
